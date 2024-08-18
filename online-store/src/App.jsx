8b4bbe0df500abadbdc3b5a1a22cddac8a2e6491
Kyle Pickard <kylepickard95@gmail.com>
2024-08-18 15:45:31 -0700

    CRUD functions

--- online-store/src/App.jsx
+++ online-store/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import Shop from "./components/Shop";
@@ -10,7 +10,25 @@ function App() {
   const [shoppingCart, setShoppingCart] = useState([]);
   const [totalItems, setTotalItems] = useState(0);
   const [openCart, setOpenCart] = useState(false);
+  const [totalPrice, setTotalPrice] = useState(0);
 
+  // Total Calculated Price
+  useEffect(() => {
+    const calculatedPrice = shoppingCart.reduce((total, item) => {
+      return total + item.price * item.quantity;
+    }, 0);
+    setTotalPrice(calculatedPrice);
+  }, [shoppingCart]);
+
+  // Total Items
+  useEffect(() => {
+    const totalItems = shoppingCart.reduce((total, product) => {
+      return total + product.quantity;
+    }, 0);
+    setTotalItems(totalItems);
+  }, [shoppingCart]);
+
+  // ADd Shopping Cart Item
   function onAddToShoppingCart(productData) {
     setShoppingCart((oldData) => {
       const productIdx = oldData.findIndex(
@@ -34,11 +52,40 @@ function App() {
         ];
       }
     });
-    setTotalItems((prevState) => prevState + 1);
   }
 
+  // Delete From Cart
+  function onDeleteFromShoppingCar(productId) {
+    setShoppingCart((prevCart) =>
+      prevCart.filter((product) => product.id !== productId)
+    );
+  }
+
+  // Toggle Shopping Cart State
   function toggleShoppingCart() {
     setOpenCart((prevState) => !prevState);
+  }
+
+  // Add Quantity to Item
+  function onAddQuantity(productId) {
+    setShoppingCart((prevState) =>
+      prevState.map((product) =>
+        product.id === productId
+          ? { ...product, quantity: product.quantity + 1 }
+          : product
+      )
+    );
+  }
+
+  // Decrement Quantity to Item
+  function onDecrementQuantity(productId) {
+    setShoppingCart((prevState) =>
+      prevState.map((product) =>
+        product.id === productId
+          ? { ...product, quantity: product.quantity - 1 }
+          : product
+      )
+    );
   }
 
   console.log("Shopping Cart: ", shoppingCart);
@@ -49,6 +96,10 @@ function App() {
         <Cart
           shoppingCart={shoppingCart}
           toggleShoppingCart={toggleShoppingCart}
+          calculatedPrice={totalPrice}
+          onAddQuantity={onAddQuantity}
+          onDecrementQuantity={onDecrementQuantity}
+          onDelete={onDeleteFromShoppingCar}
         />
       ) : (
         <>
--- online-store/src/components/Cart.jsx
+++ online-store/src/components/Cart.jsx
@@ -1,4 +1,11 @@
-const Cart = ({ shoppingCart, toggleShoppingCart }) => {
+const Cart = ({
+  shoppingCart,
+  toggleShoppingCart,
+  calculatedPrice,
+  onAddQuantity,
+  onDecrementQuantity,
+  onDelete,
+}) => {
   return (
     <div>
       <h1 className="font-bold text-3xl mb-5 mt-10 text-white">
@@ -13,47 +20,72 @@ const Cart = ({ shoppingCart, toggleShoppingCart }) => {
             {/* Product Map */}
 
             <ul className="text-white">
-              {shoppingCart.map((cartProduct) => (
-                <li key={cartProduct.id}>
-                  {/* Top Border */}
-                  <div className="border border-gray-200 " />
+              {shoppingCart.map(
+                (cartProduct) =>
+                  cartProduct.quantity > 0 && (
+                    <li key={cartProduct.id}>
+                      {/* Top Border */}
+                      <div className="border border-gray-200 " />
 
-                  {/* Product */}
-                  <div className="flex flex-row items-center gap-5 my-10">
-                    {/* Product Image */}
-                    <img
-                      className="w-[200px]"
-                      src={cartProduct.image}
-                      alt={cartProduct.product}
-                    />
+                      {/* Product */}
+                      <div className="flex flex-row items-center gap-5 my-10">
+                        {/* Product Image */}
+                        <img
+                          className="w-[200px]"
+                          src={cartProduct.image}
+                          alt={cartProduct.product}
+                        />
 
-                    {/* Product Info and Quantity */}
-                    <div className="flex flex-col">
-                      <h4>{cartProduct.product}</h4>
-                      <p className="flex flex-row font-bold gap-3 mt-5">
-                        Price: ${cartProduct.price}
-                      </p>
-                      <p className="flex flex-row font-bold gap-3 mt-5">
-                        Quantity: <button>-</button> {cartProduct.quantity}
-                        <button>+</button>
-                      </p>
-                    </div>
-                  </div>
-                </li>
-              ))}
+                        {/* Product Info and Quantity */}
+                        <div className="flex flex-col items-start">
+                          <h4>{cartProduct.product}</h4>
+                          <p className="flex flex-row font-bold gap-3 mt-5">
+                            Price: ${cartProduct.price}
+                          </p>
+                          <p className="flex flex-row items-center font-bold gap-3 mt-5">
+                            Quantity:
+                            <button
+                              className="hover:text-stone-400 text-2xl"
+                              onClick={() =>
+                                onDecrementQuantity(cartProduct.id)
+                              }
+                            >
+                              -
+                            </button>
+                            {cartProduct.quantity}
+                            <button
+                              className="hover:text-stone-400 text-xl"
+                              onClick={() => onAddQuantity(cartProduct.id)}
+                            >
+                              +
+                            </button>
+                          </p>
+                          <button
+                            className="mt-5 font-bold text-red-700 hover:text-red-500"
+                            onClick={() => onDelete(cartProduct.id)}
+                          >
+                            Delete
+                          </button>
+                        </div>
+                      </div>
+                    </li>
+                  )
+              )}
             </ul>
             <div>
               {/* Subtotal */}
 
               <div className="border border-gray-200 " />
-              <p className="text-2xl mt-5 text-white">Subtotal:</p>
+              <p className="text-2xl mt-5 text-white">
+                Subtotal: ${calculatedPrice}
+              </p>
             </div>
           </div>
         </>
       )}
       <button
         onClick={toggleShoppingCart}
-        className="text-white font-bold mt-10"
+        className="text-white font-bold mt-10 underline hover:text-stone-400"
       >
         Back to shopping
       </button>
